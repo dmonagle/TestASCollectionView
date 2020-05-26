43fadcdc8a2cdbe6e4f4f3bade17154c951b6470
David Monagle <david@monagle.com.au>
2020-05-26 17:37:10 +1000

    Initial Commit

---
+++ TestASCollectionView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestASCollectionView
+//
+//  Created by David Monagle on 26/5/20.
+//  Copyright © 2020 David Monagle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
